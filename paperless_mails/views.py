eb03a7ca03a56d703ef30d3dd9af84d09463472d
markmysourcecode <markusemaibox@gmail.com>
2018-03-04 04:57:49 +0800

    Trying sendgrid

--- paperless_mails/views.py
+++ paperless_mails/views.py
@@ -4,6 +4,11 @@ from paperless_mails.forms import EmailSubscriberForm
 from paperless_mails.models import EmailSubscriber
 from application_events import application_functions as af
 
+# sendgrid settings
+import sendgrid
+import os
+from sendgrid.helpers.mail import *
+
 
 def Index_With_Email_Subscription(request):
 
@@ -22,8 +27,27 @@ def Index_With_Email_Subscription(request):
             email = form.cleaned_data.get('subscriber_email')
             EmailSubscriber.objects.create(email=email)
 
-            #print("email is valid")
-            return redirect('application_events:thanksfor', thanks_for = 'subscribed')
+
+
+            # send email via sendgrid
+            SENDGRID_API_KEY_Z = os.environ.get('SENDGRID_API_KEY')
+            print(SENDGRID_API_KEY_Z)
+            sg = sendgrid.SendGridAPIClient(apikey=SENDGRID_API_KEY_Z)
+            from_email = Email("pinneventofficial@gmail.com")
+            subject = "Greetings from Pinnevent"
+            to_email = Email(email)
+            content = Content("text/plain", "Hello, Pinner! Thanks for having interest with what we are working. We'll connect to you soon.")
+            mail = Mail(from_email, subject, to_email, content)
+            response = sg.client.mail.send.post(request_body=mail.get())
+            print(response.status_code)
+            print(response.body)
+            print(response.headers)
+
+            # return message base from success of sending email
+            if response.status_code == '202':
+                return redirect('application_events:thanksfor', thanks_for = 'subscribed')
+            else:
+                return redirect('application_events:thanksfor', thanks_for = 'subscribed_failed')
 
 
     else:
