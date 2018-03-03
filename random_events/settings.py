e93749da0ab25374d58a29df55f018e2035c007a
markmysourcecode <markusemaibox@gmail.com>
2018-03-03 23:41:54 +0800

    add statisfiles_dir

--- random_events/settings.py
+++ random_events/settings.py
@@ -183,7 +183,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-#STATICFILES_DIRS = []
+STATICFILES_DIRS = [STATIC_DIR]
 '''STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
